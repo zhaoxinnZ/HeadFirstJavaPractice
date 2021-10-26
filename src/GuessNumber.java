db2c8335906c10917094d9fcc85c49ee133a9150
zhaoxinnZ <92992703+zhaoxinnZ@users.noreply.github.com>
2021-10-26 11:02:38 +0800

    GuessNumber v0.1

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -1,28 +1,56 @@
+import java.util.Random;
 import java.util.Scanner;
+import java.security.SecureRandom;
+// Using SecureRandom because Intstream generates Integer(Int),not int,it's different,
+// int is a Type;Interger is a Object
+
 public class GuessNumber {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter a number");
-        int guesslimit = 5;
-        int i = 0;
-        int guessgoal = 50;
-        while (i <= (guesslimit - 1)) {
-            int guess = sc.nextInt();
-            if (guess < guessgoal) {
-                System.out.println("Input number is too small");
-            } else if (guess > guessgoal) {
-                System.out.println("Input number is too big");
-            } else {
-                System.out.println("You guessed the right number");
-                System.out.println("Game Over,You won!");
-                break;
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            Random random = new Random();
+
+            System.out.println("Please enter the minimum number");
+            int GuessMinimum = sc.nextInt();
+            //get minimun int
+
+            System.out.println("Please enter the maximum number");
+            int GuessMaximum = sc.nextInt();
+            //get maximum int
+
+            SecureRandom rand = new SecureRandom();
+            int GuessGoal = rand.nextInt(GuessMaximum - GuessMinimum + 1) + GuessMinimum;
+            //generate a int between GuessMinimum and GuessMaximum
+            //Caution:Guess Minimun have to be outside of the nextInt function
+
+            if (GuessMinimum < GuessMaximum) {
+                //make sure Minimum won't be greater than Maximum
+
+                int i = 0;
+
+                int GuessTimeLimit = 5;
+
+                while (i <= (GuessTimeLimit - 1)) {
+                    int guess = sc.nextInt();
+
+                    if (guess < GuessGoal) {
+                        System.out.println("Input number is too small");
+
+                    } else if (guess > GuessGoal) {
+                        System.out.println("Input number is too big");
+
+                    } else {
+                        System.out.println("You guessed the right number");
+                        System.out.println("Game Over,You won!");
+
+                        break;
+                    }
+
+                    i++;
+
+                }
+                System.out.println("The true answer is " + GuessGoal);
             }
-            i++;
-        };
-
-
+        }
     }
-
 }
-
-
