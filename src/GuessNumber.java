bd6ee41cd561c149eb6fe65d90a1100b7c9f3282
zhaoxinnZ <92992703+zhaoxinnZ@users.noreply.github.com>
2021-10-26 14:46:23 +0800

    Update GuessNumber.java

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -4,6 +4,8 @@ import java.security.SecureRandom;
 // Using SecureRandom because Intstream generates Integer(Int),not int,it's different,
 // int is a Type;Interger is a Object
 
+//There's a issue that the number randomly generated will lower than the Minimum
+
 public class GuessNumber {
     public static void main(String[] args) {
         while (true) {
